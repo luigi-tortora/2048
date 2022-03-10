7345b3f0c6964233334a415b157274f468765912
LDj3SNuD <dvitiello@gmail.com>
2022-03-10 05:51:36 +0100

    On the way!

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿public class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Grid grid = new();
-
-        Console.Title = "2048";
+        const int Max = 2048;
 
         Console.OutputEncoding = System.Text.Encoding.Unicode;
         Console.CursorVisible = false;
@@ -21,23 +23,89 @@
 
         Console.Clear();
 
+        Grid grid = new();
+
         grid.PrintInit(1, 1);
 
-/*         grid.TryFill();
+        //grid.Dbg(0, 4, 4, 2, 2); // TODO: .
+
         grid.TryFill();
-        grid.TryFill(); */
-
-        grid.Bla(0, 16, 8, 4, 2);
+        grid.TryFill();
 
         grid.PrintUpdate(1, 1);
 
-        Console.ReadLine();
+        int totalScore = 0;
+        int max = grid.GetMax();
 
-        grid.TryMove(Direction.Right);
+        Console.Title = $"- {Max} - [Score: {totalScore}] [Max: {max}]";
 
-        grid.PrintUpdate(1, 1);
+        bool exit = false;
+        GameOver gameOver = GameOver.None;
 
-        Console.ReadLine();
+        while (!exit && gameOver != GameOver.YouLose)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo cKI = Console.ReadKey(true);
+                ThreadPool.QueueUserWorkItem((_) => { while (Console.KeyAvailable && Console.ReadKey(true) == cKI); });
+
+                switch (cKI.Key)
+                {
+                    //case ConsoleKey.UpArrow: // TODO: .
+                    //case ConsoleKey.DownArrow:
+                    //case ConsoleKey.LeftArrow:
+                    case ConsoleKey.RightArrow:
+                    {
+                        if (grid.TryMove(GetDirectionByConsoleKey(cKI.Key), out int score))
+                        {
+                            grid.TryFill();
+
+                            grid.PrintUpdate(1, 1);
+
+                            totalScore += score;
+                            max = grid.GetMax();
+
+                            if (max != Max) // max < Max || max > Max
+                            {
+                                Console.Title = $"- {Max} - [Score: {totalScore}] [Max: {max}]";
+
+                                Program.BeepAsync(frequencyA: score == 0 ? 800 : 1000, duration: 250);
+                            }
+                            else // max == Max
+                            {
+                                gameOver = GameOver.YouWin;
+
+                                Console.Title = $"- {Max} - [Score: {totalScore}] [You Win!]";
+
+                                BeepAsync(frequencyA: 1000, frequencyB: 1500, duration: 500, ramp: true);
+                            }
+                        }
+                        else
+                        {
+                            if (false) // TODO: Simulate TryMove() for the other directions; if they all return false:
+                            {
+                                gameOver = GameOver.YouLose;
+
+                                Console.Title = $"- {Max} - [Score: {totalScore}] [Max: {max}] [Game Over: You Lose!]";
+
+                                BeepAsync(frequencyA: 1000, frequencyB: 500, duration: 500, ramp: true);
+
+                                while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                            }
+                        }
+
+                        break;
+                    }
+
+                    case ConsoleKey.Escape:
+                    {
+                        exit = true;
+
+                        break;
+                    }
+                }
+            }
+        }
 
         Console.OutputEncoding = System.Text.Encoding.Default;
         Console.CursorVisible = true;
@@ -48,8 +116,20 @@
         Console.ResetColor();
     }
 
-    public static void Write(string str, int left, int top, 
-        ConsoleColor foregroundColor = ConsoleColor.Gray, 
+    public static Direction GetDirectionByConsoleKey(ConsoleKey consoleKey)
+    {
+        return consoleKey switch
+        {
+            ConsoleKey.UpArrow    => Direction.Up,
+            ConsoleKey.DownArrow  => Direction.Down,
+            ConsoleKey.LeftArrow  => Direction.Left,
+            ConsoleKey.RightArrow => Direction.Right,
+            _ => throw new ArgumentException(nameof(consoleKey))
+        };
+    }
+
+    public static void Write(string str, int left, int top,
+        ConsoleColor foregroundColor = ConsoleColor.Gray,
         ConsoleColor backgroundColor = ConsoleColor.Black)
     {
         Console.ForegroundColor = foregroundColor;
@@ -60,18 +140,58 @@
 
         Console.ResetColor();
     }
+
+    public static void BeepAsync(int frequencyA = 800, int frequencyB = 0, int duration = 200, bool ramp = false)
+    {
+        ThreadPool.QueueUserWorkItem((_) =>
+        {
+            if (!ramp)
+            {
+                if (frequencyA != 0)
+                {
+                    Console.Beep(frequencyA, frequencyB == 0 ? duration : duration / 2);
+                }
+
+                if (frequencyB != 0)
+                {
+                    Console.Beep(frequencyB, frequencyA == 0 ? duration : duration / 2);
+                }
+            }
+            else
+            {
+                const int Steps = 4;
+
+                int frequencyStep = (frequencyA - frequencyB) / (Steps - 1);
+                duration /= Steps;
+
+                for (int i = 1; i <= Steps; i++)
+                {
+                    if (frequencyA != 0)
+                    {
+                        Console.Beep(frequencyA, duration);
+                    }
+
+                    frequencyA -= frequencyStep;
+                }
+            }
+        });
+    }
 }
 
-public enum Direction {Up, Right, Down, Bottom};
+public enum Direction { Up, Down, Left, Right };
+
+public enum GameOver { None, YouWin, YouLose }
 
 public class Grid
 {
     public const int GridWidth = 29;
     public const int GridHeight = 17;
     public const int Size = 4;
+
     private readonly int[,] _grid;
+
     private readonly Random _rnd;
-    private readonly List<int> _list;
+    private readonly List<int> _lst;
 
     public Grid()
     {
@@ -86,33 +206,24 @@ public class Grid
         }
 
         _rnd = new();
-        _list = new();
+        _lst = new();
     }
 
-    private int GetEmptyCells()
+    public void Dbg(int y, int a, int b, int c, int d) // TODO: .
     {
-        int emptyCells = 0;
-
-        for (int y = 0; y < Size; y++)
-        {
-            for (int x = 0; x < Size; x++)
-            {
-                if (_grid[y, x] == 0)
-                {
-                    emptyCells++;
-                }
-            }
-        }
-
-        return emptyCells;
+        _grid[y, 0] = a;
+        _grid[y, 1] = b;
+        _grid[y, 2] = c;
+        _grid[y, 3] = d;
     }
 
-    public void Bla(int y, int v0, int v1, int v2, int v3)
+    public bool TryMove(Direction direction, out int score, bool simulate = false) // TODO: Try & simulate.
     {
-        _grid[y, 0] = v0;
-        _grid[y, 1] = v1;
-        _grid[y, 2] = v2;
-        _grid[y, 3] = v3;
+        SeparationStep(direction);
+        MergingStep(direction, out score);
+        SeparationStep(direction);
+
+        return true;
     }
 
     public bool TryFill()
@@ -138,33 +249,69 @@ public class Grid
         return false;
     }
 
-    private void OrderCells(Direction direction)
+    public int GetMax()
     {
-        _list.Clear();
+        int max = 0;
 
+        for (int y = 0; y < Size; y++)
+        {
+            for (int x = 0; x < Size; x++)
+            {
+                max = Math.Max(max, _grid[y, x]);
+            }
+        }
+
+        return max;
+    }
+
+    private void SeparationStep(Direction direction) // of non-zero values, preserving their order, from the others.
+    {
         switch(direction)
         {
+            case Direction.Up:
+            {
+                // TODO: .
+
+                break;
+            }
+
+            case Direction.Down:
+            {
+                // TODO: .
+
+                break;
+            }
+
+            case Direction.Left:
+            {
+                // TODO: .
+
+                break;
+            }
+
             case Direction.Right:
             {
                 for (int y = 0; y < Size; y++)
                 {
+                    _lst.Clear();
+
                     for (int x = 0; x < Size; x++)
                     {
                         int value = _grid[y, x];
 
                         if (value != 0)
                         {
-                            _list.Add(value);
+                            _lst.Add(value);
                         }
                         else
                         {
-                            _list.Insert(0, 0);
+                            _lst.Insert(0, 0);
                         }
                     }
 
                     for (int x = 0; x < Size; x++)
                     {
-                        _grid[y, x] = _list[x];
+                        _grid[y, x] = _lst[x];
                     }
                 }
 
@@ -173,27 +320,52 @@ public class Grid
         }
     }
 
-    private void OrderStep2(Direction direction)
+    private void MergingStep(Direction direction, out int score) // of adjacent pairs of equal values.
     {
-        _list.Clear();
+        score = 0;
 
         switch(direction)
         {
+            case Direction.Up:
+            {
+                // TODO: .
+
+                break;
+            }
+
+            case Direction.Down:
+            {
+                // TODO: .
+
+                break;
+            }
+
+            case Direction.Left:
+            {
+                // TODO: .
+
+                break;
+            }
+
             case Direction.Right:
             {
                 for (int y = 0; y < Size; y++)
                 {
-                    for (int x = Size - 1; x > 0; x--)
-                    {
-                        int value1 = _grid[y, x];
-                        int value2 = _grid[y, x - 1];
+                    _lst.Clear();
 
-                        if (value1 == value2)
+                    for (int x = Size - 2; x >= 0; x--)
+                    {
+                        int left  = _grid[y, x];
+                        int right = _grid[y, x + 1];
+
+                        if (left == right)
                         {
-                            _grid[y, x] = value1 + value2;
-                            _grid[y, x - 1] = 0;
+                            _grid[y, x] = 0;
+                            _grid[y, x + 1] = left + right;
 
                             x--;
+
+                            score += left + right;
                         }
                     }
                 }
@@ -203,13 +375,22 @@ public class Grid
         }
     }
 
-    public bool TryMove(Direction direction)
+    private int GetEmptyCells()
     {
-        OrderCells(direction); //Primo Step
-        OrderStep2(direction); //Secondo Step
-        OrderCells(direction); //Terzo Step
+        int emptyCells = 0;
 
-        return true; //!Old PlayField
+        for (int y = 0; y < Size; y++)
+        {
+            for (int x = 0; x < Size; x++)
+            {
+                if (_grid[y, x] == 0)
+                {
+                    emptyCells++;
+                }
+            }
+        }
+
+        return emptyCells;
     }
 
     public void PrintInit(int left, int top)
@@ -233,7 +414,7 @@ public class Grid
         Program.Write("╚══════╧══════╧══════╧══════╝", left, top + 16);
     }
 
-    public void PrintUpdate(int left, int top)
+    public void PrintUpdate(int left, int top) // TODO: Centered values & Colors.
     {
         for (int y = 0; y < Size; y++)
         {
